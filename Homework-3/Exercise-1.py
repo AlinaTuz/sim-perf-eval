60145d8f19ffc7c4225dca3c607608baabfeedd5
Valentin PORTAIL <valentin.portail@etu.uca.fr>
2025-05-15 19:24:18 +0200

    Homework 3: Task 4

--- Homework-3/Exercise-1.py
+++ Homework-3/Exercise-1.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Random generator
+rng = np.random.Generator(np.random.MT19937(np.random.SeedSequence(1234)))
+
 # Load the data
 data = pd.read_csv('./Homework-3/data_ex1_wt.csv', header=None, names=['Time', 'Measurement'])
 x = data['Time'].values
@@ -16,6 +19,46 @@ def fit_polynomial_least_squares(x, y, degree):
     Xty = X.T @ y
     coeffs = np.linalg.solve(XtX, Xty)
     return coeffs
+
+# Expectation-Maximization algorithm
+def expectation_maximization(data, nb_dist, nb_iter):
+    # Initial assumption
+    dists = []
+    for d in range(nb_dist):
+        dists.append({'mean': (nb_dist / 2) - d, 'var': 1, 'prob': 1/3})
+
+    # Iterative computation of the parameters
+    for i in range(nb_iter):
+        print(i)
+
+        likelihoods = []
+        assignments = [[] for _ in range(nb_dist)]
+
+        for x in range(len(data)):
+
+            # Computation of likelihoods
+            probs_by_dist = []
+            for d in range(nb_dist):
+                probs_by_dist.append(dists[d]['prob'] / (np.sqrt(2 * np.pi * dists[d]['var']))
+                                     * np.exp(-((data[x] - dists[d]['mean'])**2) / (2 * dists[d]['var'])))   
+            likelihoods.append([probs_by_dist[k] / sum(probs_by_dist) for k in range(nb_dist)])
+
+            # Assignment of a Gaussian distribution for each point
+            r = rng.uniform()
+            d = 0
+            cdf = likelihoods[x][d]
+            while d < nb_dist and r > cdf:
+                d += 1
+                cdf += likelihoods[x][d]
+            assignments[d].append(data[x])
+
+        # Re-estimation of parameters
+        for d in range(nb_dist):
+            dists[d]['mean'] = np.mean(assignments[d])
+            dists[d]['var'] = np.var(assignments[d])
+            dists[d]['prob'] = len(assignments[d]) / len(data)
+
+    return dists, assignments
 
 # Loop through polynomial degrees from 1 to 6 for fitting
 rss_values = []
@@ -102,3 +145,19 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
+# Expectation / Maximization
+dists, assignments = expectation_maximization(detrended, 3, 20)
+
+for k in range(3):
+    print(f"Gaussian distribution #{k+1}:\t Mean: {dists[k]['mean']}\t Variance: {dists[k]['var']}")
+
+plt.plot()
+plt.hist(detrended, bins=20, color='lightgray', edgecolor='black')
+plt.hist(assignments, bins=20, color=['blue', 'red', 'green'], rwidth=1/2, edgecolor='black', stacked=True)
+plt.title('Histogram of the 3 Gaussian Distributions Obtained with Expectation-Maximization Algorithm')
+plt.xlabel('Residual Value')
+plt.ylabel('Frequency')
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()

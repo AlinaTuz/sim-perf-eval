6d1d1cd321ed827ee310921cde21e69f38bdfc22
Valentin PORTAIL <valentin.portail@etu.uca.fr>
2025-06-09 15:17:39 +0200

    Homework 4: Improvement of the figure

--- Homework-4/Exercise-1.py
+++ Homework-4/Exercise-1.py
@@ -40,7 +40,7 @@ class MM1:
 
     # Report generator
 
-    def report_generator(self, display):
+    def report_generator(self, arrival_rate, departure_rate, display):
         # Compute estimates of interest
 
         avg_packets = self.nbs_packets[0] * self.times[0]
@@ -49,11 +49,16 @@ class MM1:
             avg_packets += self.nbs_packets[i] * (self.times[i] - self.times[i-1])
         
         # Write report
+        rho = arrival_rate / departure_rate
+
         if display:
-            plt.bar(x=self.times, height=self.nbs_packets, width=0.5)
+            plt.plot()
+            plt.bar(x=self.times, height=self.nbs_packets, width=0.5, label='Instantaneous system utilisation')
+            plt.axhline(y=rho/(1-rho), color="black", linestyle="--", label='Theoretical average')
             plt.title('Evolution of the number of packets in the system over time')
             plt.xlabel('Time')
             plt.ylabel('Number of packets in the system (server + queue)')
+            plt.legend()
             plt.grid(True)
             plt.show()
 
@@ -78,7 +83,7 @@ class MM1:
                 self.packet_departure(arrival_rate, departure_rate)
 
         # Report generator
-        return self.report_generator(display)
+        return self.report_generator(arrival_rate, departure_rate, display)
 
     # Event routines
 
@@ -124,10 +129,12 @@ class MM1:
 lam = 1
 mu = 2
 
+max_time = 1000
+
 # Example on one run
 
 sim = MM1()
-avg_packets = sim.run_simulation(max_time=100, arrival_rate=lam, departure_rate=mu, display=True)
+avg_packets = sim.run_simulation(max_time=max_time, arrival_rate=lam, departure_rate=mu, display=True)
 
 # Computation of the average number of packets in the system
 
@@ -138,7 +145,7 @@ avg_nb_packets = []
 
 for _ in range(nb_runs):
     sim = MM1()
-    avg_nb_packets.append(sim.run_simulation(max_time=2000, arrival_rate=lam, departure_rate=mu, display=False))
+    avg_nb_packets.append(sim.run_simulation(max_time=max_time, arrival_rate=lam, departure_rate=mu, display=False))
 
 mean = np.mean(avg_nb_packets)
 std_err = np.std(avg_nb_packets, ddof=1) / np.sqrt(nb_runs)

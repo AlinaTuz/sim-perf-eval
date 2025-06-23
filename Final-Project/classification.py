f4d6aebb210113292a25846bab6350ec3259065a
Valentin PORTAIL <valentin.portail@etu.uca.fr>
2025-06-23 10:12:08 +0200

    First example of classification testing based on the one from Scikit-Learn

--- Final-Project/classification.py
+++ Final-Project/classification.py
@@ -1,11 +1,83 @@
-# Logistic regression
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Decision tree
+import sklearn
+import sklearn.datasets
+import sklearn.ensemble
+import sklearn.model_selection
+import sklearn.naive_bayes
+import sklearn.neighbors
+import sklearn.neural_network
+import sklearn.pipeline
+import sklearn.preprocessing
+import sklearn.svm
+import sklearn.tree
 
-# Random forest
+# Inspired by:
+# https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
 
-# Support vector machine
+# Initializing some clasification algorithms
 
-# K-nearest neighbors
+names = [
+    "Decision Tree",
+    "Random Forest",
+    "Linear Support Vector Machine",
+    "RBF Support Vector Machine",
+    "K-Nearest Neighbors",
+    "Naive Bayes",
+    "Neural Network"
+]
 
-# Naive Bayes
+classifiers = [ # The following parameters may need to be fine-tuned
+    sklearn.tree.DecisionTreeClassifier(max_depth=5, random_state=44),
+    sklearn.ensemble.RandomForestClassifier(
+        max_depth=5, n_estimators=10, max_features=1, random_state=44
+    ),
+    sklearn.svm.SVC(kernel='linear', C=0.025, random_state=44),
+    sklearn.svm.SVC(gamma=2, C=1, random_state=44),
+    sklearn.neighbors.KNeighborsClassifier(3),
+    sklearn.naive_bayes.GaussianNB(),
+    sklearn.neural_network.MLPClassifier(alpha=1, max_iter=1000, random_state=44)
+]
+
+# Building a linearly separable dataset (Can be modified)
+
+X, y = sklearn.datasets.make_classification(
+    n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1
+)
+
+rng = np.random.RandomState(2)
+X += 2 * rng.uniform(size=X.shape)
+
+linearly_separable = (X, y)
+
+# Definition of some datasets (May need to be modified)
+
+datasets = [
+    sklearn.datasets.make_moons(noise=0.3, random_state=0),
+    sklearn.datasets.make_circles(noise=0.2, factor=0.5, random_state=1),
+    linearly_separable
+]
+
+# Iteration over datasets
+
+for ds_count, ds in enumerate(datasets):
+    print(f"====== Dataset #{ds_count + 1} ======")
+
+    # We preprocess our dataset and we split it into training and test part
+
+    X, y = ds
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, y, test_size=0.4, random_state=44
+    )
+
+    # Iteration over classifiers
+
+    for name, clf in zip(names, classifiers):
+        print(f"--- {name} ---")
+
+        clf = sklearn.pipeline.make_pipeline(sklearn.preprocessing.StandardScaler(), clf)
+        clf.fit(X_train, y_train)
+
+        score = clf.score(X_test, y_test)
+        print(score)
